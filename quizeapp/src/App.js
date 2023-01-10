25f4e11e776ed0a7d3cf0d71ec8a1f9474109248
Akash Jadhav <87018998+Akashjadhav55@users.noreply.github.com>
2023-01-10 16:44:31 +0530

    update

--- quizeapp/src/App.js
+++ quizeapp/src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Test from "./test.jsx"
 function App() {
   return (
     <div className="App">
-      
+      <Test/>
     </div>
   );
 }
